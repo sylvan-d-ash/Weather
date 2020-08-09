7ed2566e5c729f0a250055f8240e84589db968fa
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-09 15:38:58 +0300

    Change to a table view controller

--- Weather App II/ViewController.swift
+++ Weather App II/ViewController.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
