cae0a12b9e47a0a0c995a088f201287cc242a2d9
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-09 23:39:25 +0300

    Add cache service + a json file with forecasts

--- Weather App II/CacheService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CacheService.swift
-//  Weather App II
-//
-//  Created by Sylvan Ash on 09/08/2020.
-//  Copyright © 2020 Sylvan Ash. All rights reserved.
-//
-
-import Foundation
---
+++ Weather App II/Services/CacheService.swift
@@ -0,0 +1,30 @@
+//
+//  CacheService.swift
+//  Weather App II
+//
+//  Created by Sylvan Ash on 09/08/2020.
+//  Copyright © 2020 Sylvan Ash. All rights reserved.
+//
+
+import Foundation
+
+enum CacheError: Error {
+    case noCache
+}
+
+class CacheService: WebServiceProtocol {
+    func fetchForecast(location: String, completion: @escaping (Result<[Forecast], Error>) -> Void) {
+        guard let url = Bundle.main.url(forResource: "forecast", withExtension: "json") else {
+            completion(.failure(CacheError.noCache))
+            return
+        }
+
+        do {
+            let data = try Data(contentsOf: url)
+            let forecast = try JSONDecoder().decode(ForecastList.self, from: data)
+            completion(.success(forecast.list))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
